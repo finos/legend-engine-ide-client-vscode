62aea461a949cd1faa18c8cb29115bf4be66ccb2
Gayathri Rallapalle <87973126+gayathrir11@users.noreply.github.com>
2023-11-21 11:16:20 +0530

    Setup project (#6)
    * use latest node verion in build pipeline
    
    * remove java build scripts
    
    * Update path for storing compiled javascript files
    
    * Update gitignore
    
    * Add check for copyright header
    
    * update tsconfig.json
    
    * add prettier to project
    
    * add styelint to project
    
    * add eslint to the project
    
    * update git workflows

--- src/extension.ts
+++ src/extension.ts
@@ -1,57 +1,66 @@
-
+/**
+ * Copyright (c) 2023-present, Goldman Sachs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import * as path from 'path';
-import { workspace, ExtensionContext, languages } from 'vscode';
-
+import { workspace, type ExtensionContext, languages } from 'vscode';
 import {
-	LanguageClient,
-	LanguageClientOptions,
-	ServerOptions,
-	TransportKind,
-	Executable
+  LanguageClient,
+  type LanguageClientOptions,
+  type Executable,
 } from 'vscode-languageclient/node';
 
 let client: LanguageClient;
 
-export function activate(context: ExtensionContext) {
+export function activate(context: ExtensionContext): void {
+  languages.setLanguageConfiguration('legend', {
+    wordPattern:
+      // eslint-disable-next-line prefer-named-capture-group
+      /(-?\d*\.\d\w*)|([^`~!@#%^$&*()\-=+[{\]}\\|;:'",.<>/?\s][^`~!@#%^&*()\-=+[{\]}\\|;:'",.<>/?\s]*)/,
+    comments: {
+      lineComment: '//',
+      blockComment: ['/*', '*/'],
+    },
+  });
 
-	languages.setLanguageConfiguration('legend', {
-		wordPattern: /(-?\d*\.\d\w*)|([^`~!@#%^$&*()\-=+[{\]}\\|;:'",.<>/?\s][^`~!@#%^&*()\-=+[{\]}\\|;:'",.<>/?\s]*)/,
-		comments: {
-			lineComment: '//',
-			blockComment: ['/*', '*/']
-		}
-	});
+  const serverOptions: Executable = {
+    command: 'java',
+    args: [
+      '-jar',
+      context.asAbsolutePath(path.join('jars', 'language-server.jar')),
+    ],
+  };
 
-	const serverOptions: Executable = {
-		command: 'java', 
-		args: [ '-jar', context.asAbsolutePath(path.join('jars','language-server.jar'))]
-	};
-
-/*	const serverOptions: ServerOptions = {
+  /*	const serverOptions: ServerOptions = {
 		run:   { module: serverModule, transport: TransportKind.ipc },
 		debug: { module: serverModule, transport: TransportKind.ipc }
 	};*/
 
-	const clientOptions: LanguageClientOptions = {
-		documentSelector: [{ scheme: 'file', language: 'legend' }],
-		synchronize: { fileEvents: workspace.createFileSystemWatcher('**/*.pure')
-		}
-	};
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: 'file', language: 'legend' }],
+    synchronize: { fileEvents: workspace.createFileSystemWatcher('**/*.pure') },
+  };
 
-	client = new LanguageClient(
-		'Legend',
-		'Legend',
-		serverOptions,
-		clientOptions
-	);
+  client = new LanguageClient('Legend', 'Legend', serverOptions, clientOptions);
 
-	client.start();
+  client.start();
 }
 
 export function deactivate(): Thenable<void> | undefined {
-	if (!client) {
-		return undefined;
-	}
-	return client.stop();
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
 }
