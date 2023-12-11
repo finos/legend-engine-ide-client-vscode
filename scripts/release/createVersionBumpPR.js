979af48a612a4e9d09446b96af27b99185b94754
Gayathri <rallapalle.gayathrichowdary@ny.email.gs.com>
2023-12-11 18:54:05 +0530

    update scripts

--- scripts/release/createVersionBumpPR.js
+++ scripts/release/createVersionBumpPR.js
@@ -17,6 +17,8 @@
 const github = require('@actions/github');
 const githubActionCore = require('@actions/core');
 const chalk = require('chalk');
+const fs = require('fs');
+const { execSync } = require('child_process');
 
 const DEFAULT_BRANCH_NAME = 'main';
 const PREPARE_RELEASE_PR_BRANCH_NAME = 'bot/prepare-release';
@@ -24,9 +26,9 @@ const PREPARE_RELEASE_PR_BRANCH_NAME = 'bot/prepare-release';
 const prepareNewStandardRelease = async () => {
   const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
   const bumpType = process.env.BUMP_TYPE;
-  const PACKAGE_JSON_PATH = '../package.json';
+  const PACKAGE_JSON_FILE_NAME = 'package.json';
 
-  // Push release version bump changeset
+  // Push release version bump
   console.log(`Creating release version bump PR...`);
 
   try {
@@ -39,7 +41,7 @@ const prepareNewStandardRelease = async () => {
     // clean the PR branch just in case
     try {
       await octokit.rest.git.deleteRef({
-        ref: `heads/${CHANGESET_PR_BRANCH_NAME}`,
+        ref: `heads/${PREPARE_RELEASE_PR_BRANCH_NAME}`,
         ...github.context.repo,
       });
     } catch (e) {
@@ -55,6 +57,32 @@ const prepareNewStandardRelease = async () => {
 
     // Use npm version to update the version based on the specified type
     execSync(`npm version ${bumpType}`, { stdio: 'inherit' });
+
+    let existingPackageJsonFile;
+    let updatedPackageJsonContent;
+    try {
+      existingPackageJsonFile = (
+        await octokit.rest.repos.getContent({
+          path: PACKAGE_JSON_FILE_NAME,
+          ...github.context.repo,
+        })
+      ).data;
+      updatedPackageJsonContent = fs.readFileSync('package.json', 'base64');
+    } catch (error) {
+      githubActionCore.error(`Failed to find file. Error:\n${error.message}\n`);
+      process.exit(1);
+    }
+
+    await octokit.rest.repos.createOrUpdateFileContents({
+      path: PACKAGE_JSON_FILE_NAME,
+      message: 'prepare for new release',
+      branch: PREPARE_RELEASE_PR_BRANCH_NAME,
+      content: updatedPackageJsonContent,
+      // 'sha' is required when we update the file, i.e the changeset file exists but its content is stale
+      // See https://docs.github.com/en/rest/reference/repos#create-or-update-file-contents
+      sha: existingPackageJsonFile?.sha,
+      ...github.context.repo,
+    });
 
     const bumpVersionPR = (
       await octokit.rest.pulls.create({
