f8c0a6e417b5363b5841f3c5e1e5bed9bbf7ad8f
sameer saini <sameersaini40@gmail.com>
2024-08-01 12:48:56 -0600

    updates glob dep and README (#82)

--- src/tests/suite/index.ts
+++ src/tests/suite/index.ts
@@ -16,37 +16,34 @@
 
 import * as path from 'path';
 import * as Mocha from 'mocha';
-import * as glob from 'glob';
+import { glob } from 'glob';
 
-export function run(): Promise<void> {
-  // Create the mocha test
+export const run = (): Promise<void> => {
   const mocha = new Mocha({
     ui: 'tdd',
   });
   const testsRoot = path.resolve(__dirname, '..');
 
-  return new Promise((c, e) => {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    glob('**/**.test.js', { cwd: testsRoot }, (err: any, files: any[]) => {
-      if (err) {
-        return e(err);
-      }
-      // Add files to the test suite
-      files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
+  return new Promise((resolve, reject) => {
+    glob('**/**.test.js', { cwd: testsRoot })
+      .then((files: string[]) => {
+        // Add files to the test suite
+        files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
 
-      try {
-        // Run the mocha test
-        mocha.run((failures) => {
-          if (failures > 0) {
-            e(new Error(`${failures} tests failed.`));
-          } else {
-            c();
-          }
-        });
-      } catch (error) {
-        e(error);
-      }
-      return undefined;
-    });
+        try {
+          // Run the mocha test
+          mocha.run((failures) => {
+            if (failures > 0) {
+              reject(new Error(`${failures} tests failed.`));
+            } else {
+              resolve();
+            }
+          });
+        } catch (error) {
+          reject(error);
+        }
+        return undefined;
+      })
+      .catch((err) => reject(err));
   });
-}
+};
