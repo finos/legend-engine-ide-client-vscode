807f7f08b36c2094f2d9158b01c68691af042881
Travis Stebbins <travis.stebbins@ny.email.gs.com>
2025-01-15 19:08:41 -0500

    Testing PR: publish to npm (#139)
    * feat: Allow passing additional presets/plugins to ComponentRouter
    
    * feat: Update package.json name and include exports, module, and types
    
    * feat: Add .npmignore file
    
    * feat: Add src/index.ts file to export only ComponentRouter
    
    * feat: Add publish to NPM step to manual__release action
    
    * fix: Fix copyright
    
    * feat: Add /src/extension and /lib/extension to .npmignore
    
    * refactor: Reorganize code into separate packages
    
    * feat: Update .gitignore
    
    * feat: Remove unused dependencies from child packages
    
    * fix: Fix broken style import
    
    * fix: Fix webpack config
    
    * feat: Use * for workspace dependency version
    
    * feat: Add scripts to root package.json
    
    * feat: Move scripts to extension folder and update package.json and tsconfig
    
    * feat: Update .gitignore
    
    * feat: Add launch.json for debugging extension
    
    * feat: Remove accidentally included lib files
    
    * feat: Remove accidentally included lib files
    
    * feat: Update package commands
    
    * feat: Remove .js from exports
    
    * feat: use bundler and adjust webpack configs
    
    * feat: Update package-lock.json
    
    * feat: Remove vscode specifics from TextLocation and TextInterval
    
    * feat: Remove unnecessary configs from rollup config
    
    * refactor: Remove tsconfig.tsbuildinfo file
    
    * feat: Set tsBuildInfoFile in tsconfig.json files
    
    * feat: Remove unused externals from rollup config
    
    * feat: Update bundle command in root package.json
    
    * feat: Remove sass-loader from webpack config
    
    * feat: Move extension config into root package.json
    
    * refactor: Fix rollup config formatting
    
    * fix: Update webpack config to include all entries
    
    * feat: Update launch.json to use root directory
    
    * feat: Re-add sass-loader to webpack webviewConfig
    
    * fix: Fix paths to dist js files
    
    * feat: Re-add sass-loader to webpack webviewConfig
    
    * refactor: Remove commented lines from webpack config
    
    * refactor: Reformat tsconfig files
    
    * feat: Switch from npm to yarn
    
    * feat: Update @finos/legend-vscode-extension-dependencies version
    
    * feaet: Set hoistingLimits on extension package.json
    
    * feat: Add shared dependencies to shared package.json and update scripts to use yarn
    
    * feat: Update client package.json to use yarn in scripts
    
    * refactor: Move extension config back to extension package.json
    
    * feat: Add version command to sub packages
    
    * feat: Update manual release workflow to use yarn and new package structure
    
    * feat: Fix commands/config for packaging extension
    
    * feat: Add dry-run scripts and fix webpack:package script
    
    * feat: Update manual__release flow to do dry runs
    
    * feat: Update check-build job to use yarn
    
    * feat: Update check-code job to use yarn
    
    * feat: Update cve-scanning job to use yarn
    
    * feat: Update license-scanning job to use yarn
    
    * refactor: Update comment in manual__release job"
    
    * fix: Fix bug with test script
    
    * feat: Update test workflow to use yarn
    
    * fix: Fix lint:ci script
    
    * feat: Simplify package.json scripts using yarn workspaces
    
    * feat: Run lint:ci in parallel
    
    * fix: Fix formatting
    
    * fix: Add missing copyright
    
    * fix: Fix issues with check:format/check:copyright scripts
    
    * fix: Fix error with license-scanning command
    
    * fix: Add corepack enable step to all github workflows
    
    * fix: Try adding run before test command
    
    * fix: Fix cp path when copying generated vsix artifact
    
    * fix: Bundle code before running test

---
+++ packages/extension/webpack.config.js
@@ -0,0 +1,184 @@
+/**
+ * Copyright (c) 2023-present, Goldman Sachs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const { type } = require('os');
+const path = require('path');
+const webpack = require('webpack');
+
+const webviewConfig = {
+  entry: {
+    AgGridRenderer: '../client/src/components/grid/AgGridRenderer',
+    FunctionResultsEditorRenderer:
+      '../client/src/components/function/FunctionResultsEditorRenderer',
+    WebViewRoot: '../client/src/components/WebViewRoot.tsx',
+  },
+  externals: {
+    vscode: 'commonjs vscode',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'assets/[hash][ext][query]',
+  },
+  devtool: 'nosources-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s?(a|c)ss$/,
+        use: ['style-loader', 'css-loader', { loader: 'sass-loader' }],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset',
+      },
+    ],
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+    new webpack.DefinePlugin({
+      AG_GRID_LICENSE: null,
+    }),
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.css', '.scss'],
+    fallback: {
+      stream: require.resolve('stream-browserify'),
+      crypto: require.resolve('crypto-browserify'),
+      vm: require.resolve('vm-browserify'),
+    },
+    alias: {
+      react: path.resolve('/node_modules/react'),
+      process: 'process/browser',
+    },
+  },
+};
+
+const purebookRendererConfig = {
+  entry: {
+    PurebookRendererRoot: '../client/src/purebook/PurebookRendererRoot.tsx',
+  },
+  externals: {
+    vscode: 'commonjs vscode',
+  },
+  target: 'web',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'module',
+    assetModuleFilename: 'assets/[hash][ext][query]',
+  },
+  experiments: {
+    outputModule: true,
+  },
+  devtool: 'nosources-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s?(a|c)ss$/,
+        use: ['style-loader', 'css-loader', { loader: 'sass-loader' }],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset',
+      },
+    ],
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+    new webpack.DefinePlugin({
+      AG_GRID_LICENSE: null,
+    }),
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.css', '.scss'],
+    fallback: {
+      stream: require.resolve('stream-browserify'),
+      crypto: require.resolve('crypto-browserify'),
+      vm: require.resolve('vm-browserify'),
+    },
+    alias: {
+      react: path.resolve('./node_modules/react'),
+      process: 'process/browser',
+    },
+  },
+};
+
+const extensionConfig = {
+  target: 'webworker',
+  entry: './src/extension.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'extension.js',
+    libraryTarget: 'commonjs2',
+    devtoolModuleFilenameTemplate: '../[resource-path]',
+  },
+  devtool: 'nosources-source-map',
+  externals: {
+    vscode: 'commonjs vscode',
+    'vscode-languageclient/node': 'vscode-languageclient/node',
+  },
+  resolve: {
+    mainFields: ['browser', 'module', 'main'],
+    extensions: ['.tsx', '.ts', '.js', '.css', '.scss'],
+    fallback: {
+      // see https://webpack.js.org/configuration/resolve/#resolvefallback
+      console: require.resolve('console-browserify'),
+      assert: require.resolve('assert'),
+    },
+    alias: {
+      react: path.resolve('./node_modules/react'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+          },
+        ],
+      },
+      {
+        test: /\.s?(a|c)ss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+    ],
+  },
+};
+
+module.exports = [extensionConfig, webviewConfig, purebookRendererConfig];
