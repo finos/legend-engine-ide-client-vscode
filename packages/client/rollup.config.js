3acad55258d6aa365191da42ed80621c816d0bf0
Stebbins, Travis [Engineering] <travis.stebbins@ny.email.gs.com>
2025-01-24 17:19:20 -0500

    fix: Mark react and react-dom as external in rollup configs

--- packages/client/rollup.config.js
+++ packages/client/rollup.config.js
@@ -28,7 +28,7 @@ const extensions = ['.js', '.ts', '.jsx', '.tsx'];
 
 export default {
   input: 'src/index.ts',
-  external: ['@jest/globals', 'react-dom/server'],
+  external: ['@jest/globals', 'react-dom/server', 'react', 'react-dom'],
   output: [
     {
       file: 'lib/bundles/bundle.cjs.js',
